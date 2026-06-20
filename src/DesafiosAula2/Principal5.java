1a33e61fb369d9e818fe05717e0d970ab651669c
Gustavo <gbonatto19@gmail.com>
2026-06-20 14:38:25 -0300

    Testando classe Livro

--- src/DesafiosAula2/Principal5.java
+++ src/DesafiosAula2/Principal5.java
@@ -1,2 +1,12 @@
-package DesafiosAula2;public class Principal5 {
+package DesafiosAula2;
+// Testando Livro
+public class Principal5 {
+    public static void main(String[] args) {
+        Livro livro = new Livro();
+
+        livro.setTitulo("Dom Casmurro");
+        livro.setAutor("Machado de Assis");
+
+        livro.exibirDetalhes();
+    }
 }
