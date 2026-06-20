91dac5fbfd3ad7926365c4c7309fb6dabc1189e7
Gustavo <gbonatto19@gmail.com>
2026-06-20 14:38:14 -0300

    Testando classe Produto

--- src/DesafiosAula2/Principal3.java
+++ src/DesafiosAula2/Principal3.java
@@ -1,2 +1,16 @@
-package DesafiosAula2;public class Principal3 {
+package DesafiosAula2;
+// Testando Produto
+public class Principal3 {
+    public static void main(String[] args) {
+        Produto produto = new Produto();
+
+        produto.setNome("Notebook");
+        produto.setPreco(3000);
+
+        System.out.println("Preço original: R$ " + produto.getPreco());
+
+        produto.aplicarDesconto(10);
+
+        System.out.println("Preço com desconto: R$ " + produto.getPreco());
+    }
 }
