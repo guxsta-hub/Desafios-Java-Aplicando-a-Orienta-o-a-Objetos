be2223a3a282667b929eb76eb15abdd8f416a3fd
Gustavo <gbonatto19@gmail.com>
2026-06-20 14:38:43 -0300

    Exibindo detalhes do livro

--- src/DesafiosAula2/Livro.java
+++ src/DesafiosAula2/Livro.java
@@ -1,2 +1,27 @@
-package DesafiosAula2;public class Livreo {
+package DesafiosAula2;
+// Classe Livro
+public class Livro {
+    private String titulo;
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+    }
 }
