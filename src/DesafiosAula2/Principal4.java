4026acc3576621894a4e89e23ac8d31bfdeb2d23
Gustavo <gbonatto19@gmail.com>
2026-06-20 14:38:20 -0300

    Testando classe Aluno

--- src/DesafiosAula2/Principal4.java
+++ src/DesafiosAula2/Principal4.java
@@ -1,2 +1,14 @@
-package DesafiosAula2;public class Principal4 {
+package DesafiosAula2;
+// Testando Aluno
+public class Principal4 {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+
+        aluno.setNome("Gustavo");
+        aluno.setNota1(8.5);
+        aluno.setNota2(7.5);
+
+        System.out.println("Aluno: " + aluno.getNome());
+        System.out.println("Média: " + aluno.calcularMedia());
+    }
 }
