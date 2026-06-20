2e889b09e530b8b137cafa53a2f7676637315c4d
Gustavo <gbonatto19@gmail.com>
2026-06-20 14:38:05 -0300

    Testando classe IdadePessoa

--- src/DesafiosAula2/Principal2.java
+++ src/DesafiosAula2/Principal2.java
@@ -1,2 +1,12 @@
-package DesafiosAula2;public class Principal2 {
+package DesafiosAula2;
+// Testando IdadePessoa
+public class Principal2 {
+    public static void main(String[] args) {
+        IdadePessoa pessoa = new IdadePessoa();
+
+        pessoa.setNome("Gustavo");
+        pessoa.setIdade(16);
+
+        pessoa.verificarIdade();
+    }
 }
