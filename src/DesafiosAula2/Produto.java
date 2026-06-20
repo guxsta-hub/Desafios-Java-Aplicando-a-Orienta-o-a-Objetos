8f38d43fa9373152b8385a0515d92d5bd9fb2d8d
Gustavo <gbonatto19@gmail.com>
2026-06-20 14:38:47 -0300

    Aplicando desconto em produto

--- src/DesafiosAula2/Produto.java
+++ src/DesafiosAula2/Produto.java
@@ -1,2 +1,26 @@
-package DesafiosAula2;public class Produto {
+package DesafiosAula2;
+// Classe Produto
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void aplicarDesconto(double percentual) {
+        preco -= preco * percentual / 100;
+    }
 }
